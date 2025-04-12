b08a66b1b5d92b9688fdf938446338e3a3492c9d
unknown <gabelwarega@gmail.com>
2025-04-12 15:00:10 +0300

    added the eventlisteners on non-operator btns

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // create DOM for the display
-const result = document.getElementById('display');
-const equality = document.getElementById("equals");
-const reset =  document.getElementById('clear');
+const resultBtn = document.getElementById('display');
+const equalBtn = document.getElementById("equals");
+const resetBtn =  document.getElementById('clear');
 const btns = document.querySelectorAll('.btn');
-// capture the functions
-const operatorFunction = operations();
-const numberFunction = numberValues();
+const deleteBtn = document.getElementById('delete')
+
+// EventListeners
+resetBtn.addEventListener('click',function(){
+    display.innerHTML = " ";
+});
+
+deleteBtn.addEventListener('click',function(){
+    display.innerHTML = 0;
+});
+
+equalBtn.addEventListener('click',function(){
+    display.innerHTML = operations();
+})
 
 
 // loop through the buttons
@@ -13,29 +24,18 @@ btns.forEach((btn) => {
     btns.addEventListener('click', () => {
         // check if the button is a number or an operator
         if(btn.classList.contains('number')){
-            result.textContent = numberFunction;
+            resultBtn.textContent += number;
         }
         else if(btn.classList.contains('operator')){
-            result.textContent = operatorFunction;
+            resultBtn.textContent += operator;
         }
     })
 })
 
-
-//create function for numbers
-function numberValues(){
-    // loop through the numbers
-    let values = [1,2,3,4,5,6,7,8,9]
-    for(let i = 0; i <values.length; i++){
-        return values[i];
-    }
-}
-numberValues()
-
 // create a function to handle the operators.
 function operations(){
     let num = 1;
-    let operators = ["+","-","/","*","-"]
+    let operators = ["+","-","/","*","="]
     // loop through
     for(let i = 0; i < operators.length; i++){
         if(operators[i] === "+"){
