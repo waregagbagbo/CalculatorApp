ad9f9ec37cd708e8ef9c2ea25ecc5bcd9726d5ec
unknown <gabelwarega@gmail.com>
2025-04-12 13:48:18 +0300

    added the function for numbers

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 const result = document.getElementById('display');
 const equality = document.getElementById("equals");
 const reset =  document.getElementById('clear');
+const btns = document.querySelectorAll('.btn');
+result.innerHTML = numberValues()
 
-
+//create function for numbers
+function numberValues(){
+    // loop through the numbers
+    let values = [1,2,3,4,5,6,7,8,9]
+    for(let i = 0; i <values.length; i++){
+        return values[i]
+    }
+}
+numberValues
 
 
 
