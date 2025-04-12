8162d03857ba2b18e0f749e6e991d86a2a674698
unknown <gabelwarega@gmail.com>
2025-04-12 14:15:57 +0300

    removed the duplicate innerHTML

--- index.js
+++ index.js
@@ -3,21 +3,34 @@ const result = document.getElementById('display');
 const equality = document.getElementById("equals");
 const reset =  document.getElementById('clear');
 const btns = document.querySelectorAll('.btn');
-result.innerHTML = numberValues()
+// capture the functions
+const operatorFunction = operations();
+const numberFunction = numberValues();
+
+
+// loop through the buttons
+btns.forEach((btn) => {
+    btns.addEventListener('click', () => {
+        // check if the button is a number or an operator
+        if(btn.classList.contains('number')){
+            result.textContent = numberFunction;
+        }
+        else if(btn.classList.contains('operator')){
+            result.textContent = operatorFunction;
+        }
+    })
+})
+
 
 //create function for numbers
 function numberValues(){
     // loop through the numbers
     let values = [1,2,3,4,5,6,7,8,9]
     for(let i = 0; i <values.length; i++){
-        return values[i]
+        return values[i];
     }
 }
-numberValues
-
-
-
-
+numberValues()
 
 // create a function to handle the operators.
 function operations(){
