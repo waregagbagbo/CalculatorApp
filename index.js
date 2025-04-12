5241816af57a9b85b5d668b4df3ba0fac20aa023
unknown <gabelwarega@gmail.com>
2025-04-12 15:57:40 +0300

    fixed the display issue

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
 // create DOM for the display
-const resultBtn = document.getElementById('display');
+const result = document.getElementById('display');
 const equalBtn = document.getElementById("equals");
 const resetBtn =  document.getElementById('clear');
 const btns = document.querySelectorAll('.btn');
-const deleteBtn = document.getElementById('delete')
+const deleteBtn = document.getElementById('delete');
 
 // EventListeners
 resetBtn.addEventListener('click',function(){
-    display.innerHTML = " ";
+    result.textContent = " "
 });
 
 deleteBtn.addEventListener('click',function(){
-    display.innerHTML = 0;
+    result.innerHTML = 0;
 });
 
 equalBtn.addEventListener('click',function(){
-    display.innerHTML = operations();
-})
-
+    try{
+        result.textContent = operations();
+    }
+    catch{
+        result.textContent = "Error"
+    }
+});
 
 // loop through the buttons
-btns.forEach((btn) => {
-    btns.addEventListener('click', () => {
+/*btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
         // check if the button is a number or an operator
         if(btn.classList.contains('number')){
-            resultBtn.textContent += number;
+            result.textContent += btn.textContent;
         }
         else if(btn.classList.contains('operator')){
-            resultBtn.textContent += operator;
+            result.innerHTML += operator;
         }
     })
+})*/
+btns.forEach((btn) =>{
+    btn.addEventListener('click', function(){
+        result.textContent += btn.textContent;
+    })
 })
-
 // create a function to handle the operators.
 function operations(){
-    let num = 1;
-    let operators = ["+","-","/","*","="]
-    // loop through
-    for(let i = 0; i < operators.length; i++){
-        if(operators[i] === "+"){
+    let num = 0;
+    let operator;
+    // set conditions for the operations
+        if(operator === "+"){
             return num + num;
         }
-        else if(operators[i] === "-"){
+        else if(operator === "-"){
             return num - num;
         }
-        else if(operators[i] === "/"){
+        else if(operator === "/"){
             return num / num;
         }
-        else if(operators[i] === "*"){
+        else if(operator === "*"){
             return num * num;
         }
-        else if(operators[i] === "-"){
+        else if(operator === "-"){
             return num - num;
         }
     }
-}
 operations()
